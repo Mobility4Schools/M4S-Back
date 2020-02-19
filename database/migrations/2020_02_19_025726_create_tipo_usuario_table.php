dcb94848b2159bab4f737f026760a16cce214ffc
jhonatan Leon Leon <jhonatan1085@gmail.com>
2020-02-18 21:58:56 -0500

    migracion tabla tipo usuario

---
+++ database/migrations/2020_02_19_025726_create_tipo_usuario_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipoUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_usuario', function (Blueprint $table) {
+            $table->bigIncrements('id_tUsuario');
+            $table->string('tu_nombre',50);
+            $table->char('tu_status',1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_usuario');
+    }
+}
